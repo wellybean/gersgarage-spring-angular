33297decea754800eebd3bd489da401b2b7fc70e
Wellington Regis <wellington.regis@gmail.com>
2021-02-14 21:57:15 +0000

    More unit tests

---
+++ server/src/test/java/com/wellybean/gersgarage/service/UserServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.wellybean.gersgarage.service;
+
+import com.wellybean.gersgarage.repository.UserRepository;
+import static com.wellybean.gersgarage.util.Constants.VALID_ID;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceImplTest {
+
+    @InjectMocks
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    public void test_findById_succeeds() {
+        userService.findById(VALID_ID);
+        verify(userRepository).findById(VALID_ID);
+    }
+}
