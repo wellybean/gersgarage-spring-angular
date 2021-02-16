ffd52d26b171d75dd699788f35203fade2157b3f
Wellington Regis <wellington.regis@gmail.com>
2021-02-16 00:49:57 +0000

    Configured swagger 2

---
+++ server/src/main/java/com/wellybean/gersgarage/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.wellybean.gersgarage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.wellybean.gersgarage.controller"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData() {
+        return new ApiInfo(
+                "Spring Boot REST API",
+                "Spring Boot REST API for Ger's Garage",
+                "1.0",
+                "Terms of service",
+                new Contact("Wellington Regis", "https://wellybean.github.io/", "wellington.regis@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0");
+    }
+}
